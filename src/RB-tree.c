b0cf1a32d23ce3eb80b1b4ccd713a4c1f033a545
hzwangxunjue <hzwangxunjue@corp.netease.com>
2015-08-24 20:04:32 +0800

    init commit

---
+++ src/RB-tree.c
@@ -0,0 +1,23 @@
+#include "RB-tree.h"
+#include <stdio.h>
+
+
+typedef struct Node{
+	int data;
+	struct Node *pLeft;
+	struct Node *pRight;
+}Node;
+
+typedef struct RB_tree{
+	Node *root;
+
+}RB_tree;
+
+
+int main(int argc, char **argv) {
+
+
+	printf("hello world!\n");
+}
+
+
