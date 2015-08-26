3746522b28965858dd678f0ef42c8d3c0c5b00eb
hzwangxunjue <hzwangxunjue@corp.netease.com>
2015-08-26 15:33:49 +0800

    RB-tree's insertional operation implemented

--- src/RB-tree.c
+++ src/RB-tree.c
@@ -1,5 +1,9 @@
 #include "RB-tree.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
 #define OK ((bool)1)
 #define ERR ((bool)0)
 #define NULL ((void *)0)
@@ -78,43 +82,111 @@ static inline void RIGHT_ROTATE(RB_tree *T, Node *pNodeY)
 	pNodeY->pParent = pNodeX;
 }
 
-int RB_Tree_InsertNode(RB_tree *pTree, int data)
+static void RB_Tree_InsertFixup(RB_tree *pTree, Node *pNode)
+{
+	Node *pUncleNode = NULL;
+
+	while (pNode->pParent->color == RED)/* Grandparent node must exist and it's color must be BLACK */
+	{
+		/* If parent node is grandparent's left child */
+		if (pNode->pParent->pParent->pLeft == pNode->pParent)
+		{
+			pUncleNode = pNode->pParent->pParent->pRight;
+			if (pUncleNode->color == RED) /* Case 1: uncle is red */
+			{
+				pNode->pParent->pParent->color = RED;
+				pUncleNode->color = BLACK;
+				pNode->pParent->color = BLACK;
+				pNode = pNode->pParent->pParent;
+				/* Fix-up is not end */
+			}
+			else
+			{
+				if (pNode == pNode->pParent->pRight)/* Case 2: uncle is black, Node is parent's right child */
+				{
+					pNode = pNode->pParent;
+					LEFT_ROTATE(pTree, pNode);
+				}
+				/* Case 3: uncle is black, Node is parent's left child */
+				pNode->pParent->pParent->color = RED;
+				pNode->pParent->color = BLACK;
+				pNode = pNode->pParent->pParent;
+				RIGHT_ROTATE(pTree, pNode);
+				/* Fix up should end */
+			}
+		}
+		else
+		{
+			pUncleNode = pNode->pParent->pParent->pLeft;
+			if (pUncleNode->color == RED) /* Case 1': uncle is red */
+			{
+				pNode->pParent->pParent->color = RED;
+				pUncleNode->color = BLACK;
+				pNode->pParent->color = BLACK;
+				pNode = pNode->pParent->pParent;
+				/* Fix-up is not end */
+			}
+			else
+			{
+				if (pNode == pNode->pParent->pLeft)/* Case 2': uncle is black, Node is parent's left child */
+				{
+					pNode = pNode->pParent;
+					RIGHT_ROTATE(pTree, pNode);
+				}
+				/* Case 3': uncle is black, Node is parent's right child */
+				pNode->pParent->pParent->color = RED;
+				pNode->pParent->color = BLACK;
+				pNode = pNode->pParent->pParent;
+				LEFT_ROTATE(pTree, pNode);
+			}
+		}
+		pTree->root->color = BLACK;
+	}	//while
+}
+
+void RB_Tree_InsertNode(RB_tree *pTree, int data)
 {
 	Node *tmpNode;
 	Node *NodeIter = pTree->root;
+	Node *NodeIterPre = NULL;
 
-	tmpNode = (Node *) malloc(sizof(Node));
+	tmpNode = (Node *) malloc(sizeof(Node));
 	tmpNode->data = data;
 	tmpNode->color = RED;
 	tmpNode->pLeft = tmpNode->pRight = tmpNode->pParent = NULL;
-
+	/* Fix-up is not end */
 	/* Find the position to insert */
-	while (NodeIter != NULL && NodeIter->pLeft != NULL
-			&& NodeIter->pRight != NULL)
+	while (NodeIter != NULL)
 	{
-#if 1
-		if (data >= NodeIter->pRight->data)
-			NodeIter = NodeIter->pRight;
-		else if (data <= NodeIter->pLeft->data)
-			NodeIter = NodeIter->pLeft;
-		else
-			break;
-#endif
+		NodeIterPre = NodeIter;
+		NodeIter = NodeIter->data < data ? NodeIter->pLeft : NodeIter->pRight;
 	}
 	/* Now NodeIter should point to the right position to insert the node */
 
-	/* Case 1: empty tree*/
 	if (pTree->root == NULL)
-	{
+	{ /* empty tree */
 		tmpNode->color = BLACK;
 		pTree->root = tmpNode;
 	}
+	else if (data < NodeIterPre->data)
+	{
+		NodeIter->pLeft = tmpNode;
+		tmpNode->pParent = NodeIterPre;
+	}
+	else
+	{
+		NodeIter->pRight = tmpNode;
+		tmpNode->pParent = NodeIterPre;
+	}
 
+	/* After insertion, fix up the tree to conform to the five rules*/
+	RB_Tree_InsertFixup(pTree, tmpNode);
 }
 
 int main(int argc, char **argv)
 {
 
 	printf("hello world!\n");
+	return 0;
 }
 
